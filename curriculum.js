935b0588c98309478f665ff7453e6c4bb60adfcd
nazyri <mohachevezsaborio@gmail.com>
2024-05-02 11:47:11 -0600

    DetallesFinales

--- curriculum.js
+++ curriculum.js
@@ -1,18 +1,5 @@
-
-console.log(Date());
+let fechaCompleta = document.getElementById("fecha")
 let fecha = new Date();
-console.log(fecha);
-// dia del mes
-console.log(fecha.getDate());
-// dia de la semana D L M Mi J V S-> 0 1 2 3 4 5 6
-console.log(fecha.getDay());
-//mes Ene Feb Mar Abr May Jun Jul Ago Sep Oct Nov Dic -> 0 1 2 3 4 5 6 7 8 9 10 11
-console.log(fecha.getMonth());
-console.log(fecha.getFullYear());
-console.log(fecha.getHours());
-console.log(fecha.getMinutes());
-console.log(fecha.getSeconds());
-console.log(fecha.getMilliseconds());
-console.log(fecha.toString());
-console.log(fecha.toDateString());
-console.log(fecha.toLocaleString());
+
+
+ fechaCompleta.innerHTML = fecha.getDay() + "/" + fecha.getMonth() + "/" + fecha.getFullYear()
