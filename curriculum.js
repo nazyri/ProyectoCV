8975d1860cdcaa2466e4bf607d657a9f5d1029e8
nazyri <mohachevezsaborio@gmail.com>
2024-05-02 11:11:13 -0600

    UltimosDetalles

--- curriculum.js
+++ curriculum.js
@@ -1,3 +1,18 @@
-<div class="iconos" style="margin-left: 480px;">
-   <img src="/imagenes/Screenshot 2024-04-30 213542.png" width="550px" alt="iconos">
-  </div>
+
+console.log(Date());
+let fecha = new Date();
+console.log(fecha);
+// dia del mes
+console.log(fecha.getDate());
+// dia de la semana D L M Mi J V S-> 0 1 2 3 4 5 6
+console.log(fecha.getDay());
+//mes Ene Feb Mar Abr May Jun Jul Ago Sep Oct Nov Dic -> 0 1 2 3 4 5 6 7 8 9 10 11
+console.log(fecha.getMonth());
+console.log(fecha.getFullYear());
+console.log(fecha.getHours());
+console.log(fecha.getMinutes());
+console.log(fecha.getSeconds());
+console.log(fecha.getMilliseconds());
+console.log(fecha.toString());
+console.log(fecha.toDateString());
+console.log(fecha.toLocaleString());
